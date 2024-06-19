b62055b29141d06ffe0b33bf80f4cc1ebc4f3064
bytesinair <ganesh.s@3umgroup.com>
2024-06-19 17:43:54 -0400

    pilot test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",  // <=== enables static exports
-};
+// const nextConfig = {
+//     output: "export",  // <=== enables static exports
+// };
 
-export default nextConfig;
+// export default nextConfig;
