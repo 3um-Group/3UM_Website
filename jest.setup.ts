29f905174cf92102b0d1af6d9706d462597c2a2d
silpavg22 <silpavg22>
2024-07-24 12:58:58 -0500

    config changes

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env.local file
-dotenv.config({ path: './.env.local' });
+dotenv.config({ path: './.env.production' });
